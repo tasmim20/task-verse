3462b1ffd3ac4f9b3c70384061fd221dbf8c68f4
ArefinShaon <arefinshaon99@gmail.com>
2024-11-28 00:15:56 +0600

    ba

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,9 @@ export default function Home() {
   return (
     <>
       <h2>task verse</h2>
-<<<<<<< HEAD
-      <><p>ami saon</p></>
-=======
       <>
         <p>ami saon</p>
       </>
->>>>>>> efec46dab9cccf5afaa3ff44d3e041faa9f7d37a
     </>
   );
 }
